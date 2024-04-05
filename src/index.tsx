69bb9767b1806cafbea509c1398d0e7f22566338
Sanjay Kumar Kumaravelan <53309069+hashedalgorithm@users.noreply.github.com>
2024-04-06 00:25:34 +0530

    IATW setsup tailwindcssIsfsdfasdf

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import './tailwind.css';
 export * from './draupnir-root';
 export * from './draupnir-provider';
 export * from './types';
--- tsdx.config.js
+++ tsdx.config.js
@@ -1,20 +1,20 @@
 const postcss = require('rollup-plugin-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
       postcss({
-        plugins: [
-          autoprefixer(),
-          cssnano({
-            preset: 'default',
-          }),
-        ],
-        inject: false,
-        extract: !!options.writeMeta,
+        config: {
+          path: './postcss.config.js',
+        },
+        extensions: ['.css'],
+        minimize: true,
+        inject: {
+          insertAt: 'top',
+        },
       })
     );
+
     return config;
   },
 };
