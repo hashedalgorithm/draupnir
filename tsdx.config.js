35e39b686cea98ecfcc562d452cd0c547298653a
Sanjay Kumar Kumaravelan <53309069+hashedalgorithm@users.noreply.github.com>
2024-04-05 15:13:58 +0530

    IATW udpates tsdx config

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,26 +1,18 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-
 module.exports = {
-  /**
-   * @param {import('rollup/dist/rollup').InputOptions} config
-   */
   rollup(config, options) {
     config.plugins.push(
       postcss({
-        modules: true,
         plugins: [
           autoprefixer(),
           cssnano({
             preset: 'default',
           }),
         ],
-        sourceMap: true,
-        extract: !!options.writeMeta,
-        minimize: true,
         inject: false,
+        extract: !!options.writeMeta,
       })
     );
     return config;
