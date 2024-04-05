a989ccf655c788f7aa0e19014799d1a2bd38010c
Sanjay Kumar Kumaravelan <53309069+hashedalgorithm@users.noreply.github.com>
2024-04-05 12:23:08 +0530

    IATW udpates example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,52 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Sample } from '../dist';
+import { DraupnirRoot, DraupnirProvider } from '../dist';
+import { TSchema } from '../dist/types/schema';
+import "../dist/index"
 
 const App = () => {
+  const schema = {
+    title: "Sample",
+    id: 'sample',
+    conditions: [],
+    properties: {
+      fname: {
+        id: "fname",
+        type: "string"
+      },
+      lname: {
+        id: "lname",
+        type: "string"
+      },
+      gender: {
+        id: "gender",
+        type: "string",
+        widget: 'select',
+        enum: ['male', 'female', "others"]
+      },
+      isAgree: {
+        id: "isAgree",
+        type: "boolean",
+      }
+    },
+    required: ["fname"],
+    type: "object",
+    version: "0.1"
+  } satisfies TSchema
+
   return (
     <div>
-      <Sample />
+      <DraupnirRoot
+        
+        widgets={{
+          base: {},
+          nonreactive: {},
+          custom: {},
+        }}
+      >
+        <DraupnirProvider schema={schema}></DraupnirProvider>
+      </DraupnirRoot>
     </div>
   );
 };
