e83fc93c3e94e564624acbf7fb8073617d8ca92c
Sanjay Kumar Kumaravelan <53309069+hashedalgorithm@users.noreply.github.com>
2024-04-06 12:54:57 +0530

    IATW fixes radio-widget type error

--- src/widgets/custom/radio-widget.tsx
+++ src/widgets/custom/radio-widget.tsx
@@ -3,19 +3,21 @@ import { TWidgetProps } from '../../types';
 
 const RadioWidget: FC<TWidgetProps> = props => {
   return (
-    props.property?.enum &&
-    props.property.enum.map((item, index) => (
-      <input
-        key={`widgetmanager.radio.defaultradio.${props.property.id}.${index}`}
-        type="radio"
-        defaultChecked={!!props.property.default}
-        name={props.property.id}
-        readOnly={props?.property.readOnly}
-        required={props?.property.required}
-        value={item}
-        className="w-4 h-4 accent-primary"
-      />
-    ))
+    <>
+      {props.property?.enum &&
+        props.property.enum.map((item, index) => (
+          <input
+            key={`radio-widget.radio.defaultradio.${props.property.id}.${index}`}
+            type="radio"
+            defaultChecked={!!props.property.default}
+            name={props.property?.location ?? props.property.id}
+            readOnly={props?.property.readOnly}
+            required={props?.property.required}
+            value={item}
+            className="w-4 h-4 accent-primary"
+          />
+        ))}
+    </>
   );
 };
 
