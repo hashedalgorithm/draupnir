f5f57190364578725f1370410086aa35926c941b
Sanjay Kumar Kumaravelan <53309069+hashedalgorithm@users.noreply.github.com>
2024-04-06 11:12:55 +0530

    IATW impls string-widget

---
+++ src/widgets/string-widget.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { TWidgetProps } from '../types/widgets';
+import { Input } from '../components/ui/input';
+import { cn } from '../lib/tw-util';
+
+const StringWidget = (props: TWidgetProps) => {
+  return (
+    <Input
+      type="string"
+      name={props.property?.location ?? props.property?.id}
+      maxLength={props.property?.maximum}
+      minLength={props.property?.minimum}
+      defaultValue={(props.property?.default as string) ?? ''}
+      placeholder={props.property?.placeholder}
+      required={props.property?.required}
+      readOnly={props.property?.readOnly}
+      className={cn(props?.className)}
+    />
+  );
+};
+
+export { StringWidget };
