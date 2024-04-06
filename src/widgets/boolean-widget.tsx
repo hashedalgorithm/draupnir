ce4f9bede213c0db746968f141dad40cd2df011a
Sanjay Kumar Kumaravelan <53309069+hashedalgorithm@users.noreply.github.com>
2024-04-06 11:12:36 +0530

    IATW impls boolean-widget

---
+++ src/widgets/boolean-widget.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import { TWidgetProps } from '../types';
+import { Switch } from '../components/ui/switch';
+import { Label } from '../components/ui/label';
+import { cn } from '../lib/tw-util';
+
+const BooleanWidget: FC<TWidgetProps> = props => {
+  return (
+    <div className={cn('flex items-center space-x-2 w-full', props?.className)}>
+      <Switch
+        id={props.property.id}
+        defaultChecked={!!props.property.default}
+        required={props.property?.required}
+        name={props.property?.location ?? props.property?.id}
+        disabled={props.property?.disabled}
+      />
+      <Label
+        htmlFor={props.property.id}
+        aria-disabled={props.property?.disabled}
+      >
+        Airplane Mode
+      </Label>
+    </div>
+  );
+};
+
+export default BooleanWidget;
