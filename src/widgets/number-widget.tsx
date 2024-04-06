5374d34251c7235f5d41f9353d860e02b83ee7c8
Sanjay Kumar Kumaravelan <53309069+hashedalgorithm@users.noreply.github.com>
2024-04-06 11:12:44 +0530

    IATW impls number widget

---
+++ src/widgets/number-widget.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import { TWidgetProps } from '../types';
+import { Input } from '../components/ui/input';
+import { cn } from '../lib/tw-util';
+
+const NumberWidget: FC<TWidgetProps> = props => {
+  return (
+    <Input
+      type="number"
+      name={props.property?.location ?? props.property?.id}
+      max={props.property?.maximum}
+      min={props.property?.minimum}
+      defaultValue={(props.property?.default as number) ?? ''}
+      placeholder={props.property?.placeholder}
+      required={props.property?.required}
+      readOnly={props.property?.readOnly}
+      className={cn(props?.className)}
+      step={props.property?.step}
+    />
+  );
+};
+
+export default NumberWidget;
