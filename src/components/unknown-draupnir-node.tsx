e8f099fd7f9b6b308b3f25611ed24414b617edd9
Sanjay Kumar Kumaravelan <53309069+hashedalgorithm@users.noreply.github.com>
2024-04-06 11:12:20 +0530

    IATW impls unknown draupnir-node

---
+++ src/components/unknown-draupnir-node.tsx
@@ -0,0 +1,14 @@
+import { RocketIcon } from '@radix-ui/react-icons';
+import React from 'react';
+import { Alert, AlertTitle } from './ui/alert';
+
+const UnknownDraupnirNode = () => {
+  return (
+    <Alert>
+      <RocketIcon className="h-4 w-4" />
+      <AlertTitle>Unknown Widget!</AlertTitle>
+    </Alert>
+  );
+};
+
+export default UnknownDraupnirNode;
