5f0ed643123fdeb07227933b57de6a40bce9be9a
Sanjay Kumar Kumaravelan <53309069+hashedalgorithm@users.noreply.github.com>
2024-04-06 11:11:46 +0530

    IATW updates field-generator

--- src/field-generator/field-generator.tsx
+++ src/field-generator/field-generator.tsx
@@ -4,28 +4,16 @@ import { TProperties } from '../types/schema';
 
 type FieldGeneratorProps = {
   properties: TProperties;
-  required: string[];
-  readOnly: boolean;
-  title: string;
 };
 const FieldGenerator = ({ properties, ...props }: FieldGeneratorProps) => {
   return (
     <>
       {Object.values(properties).map(property => {
-        if (property.type !== 'object') {
-          return property?.widget ? (
-            <WidgetManager property={property} {...props} required={false} />
-          ) : (
-            <FieldManager property={property} {...props} required={false} />
-          );
-        } else
-          return (
-            <FieldGenerator
-              key={`formfieldgenerator.subgenerator.${property.id}.${property.title}`}
-              properties={properties}
-              {...props}
-            />
-          );
+        return property?.widget ? (
+          <WidgetManager property={property} {...props} />
+        ) : (
+          <FieldManager property={property} {...props} />
+        );
       })}
     </>
   );
