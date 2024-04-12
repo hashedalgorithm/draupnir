f322ffbe54d26b7de98ed89f79d153434e84ce72
Sanjay Kumar Kumaravelan <53309069+hashedalgorithm@users.noreply.github.com>
2024-04-12 20:18:48 +0530

    updates tsdx config with target, env

--- tsdx.config.js
+++ tsdx.config.js
@@ -3,6 +3,8 @@ const postcss = require('rollup-plugin-postcss');
 module.exports = {
   rollup(config, options) {
     config.output.format = 'cjs';
+    config.output.env = 'production';
+    config.output.target = 'browser';
     config.plugins.push(
       postcss({
         config: {
