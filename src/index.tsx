a13919ef60ec31fb9446df453189cf4776dae98d
Sanjay Kumar Kumaravelan <53309069+hashedalgorithm@users.noreply.github.com>
2024-05-25 00:52:28 +0530

    IATW schema generation logics to separate util

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,6 @@ import './tailwind.css';
 import './typography.css';
 export * from './core';
 export * from './types';
+export * from './lib';
+export { Primitive as zodPrimitive } from 'zod';
+export * from 'react-hook-form';
---
+++ src/lib/zod-util.ts
@@ -0,0 +1,158 @@
+import { startCase, set } from 'lodash';
+import { z, AnyZodObject, ZodType } from 'zod';
+import { TProperties, TProperty } from '../types';
+
+export const createSchema = (properties: TProperties) => {
+  let masterSchema = z.object({} as Record<string, any>);
+
+  Object.values(properties).forEach(property => {
+    if (property.id.split('.').length === 1) {
+      masterSchema = masterSchema.extend({
+        [property.id]: createLeafZod(property),
+      });
+    } else {
+      const toplevel = property.id.split('.').at(0);
+
+      if (!toplevel) return;
+
+      masterSchema = masterSchema.extend({
+        [toplevel]: createNestedSchema(
+          masterSchema.shape?.[toplevel] ?? masterSchema,
+          property.id
+            .split('.')
+            .filter(i => i !== toplevel)
+            .join('.'),
+          property
+        ),
+      });
+    }
+  });
+
+  return masterSchema;
+};
+
+export const createNestedSchema = (
+  root: AnyZodObject,
+  propertyId: string,
+  property: TProperty
+): ZodType => {
+  const toplevel = propertyId.split('.').at(0);
+
+  if (!toplevel) return root;
+
+  let master = root.shape?.[toplevel] ?? root;
+
+  if (propertyId.split('.').length === 1) {
+    master = master.extend({
+      [propertyId]: createLeafZod(property),
+    });
+  } else {
+    master = master.extend({
+      [toplevel]: createNestedSchema(
+        master.shape?.[toplevel] ?? master,
+        propertyId
+          .split('.')
+          .filter(i => i !== toplevel)
+          .join('.'),
+        property
+      ),
+    });
+  }
+  return master;
+};
+
+export const createLeafZod = (property: TProperty): z.ZodType<any> => {
+  switch (property.type) {
+    case 'string':
+      return addStringValidators(property);
+    case 'number':
+      return addNumberValidators(property);
+    case 'boolean':
+      return addBooleanValidators(property);
+    default:
+      return z.any();
+  }
+};
+
+export const addStringValidators = (property: TProperty) => {
+  let zod = z.string({
+    description: property?.helperText,
+    invalid_type_error: `${startCase(
+      property.label ?? property.id
+    )} should be a string`,
+    required_error: `${startCase(property.label ?? property.id)} is required!`,
+  });
+
+  if (property?.readOnly) return zod.readonly();
+
+  if (property?.widget === 'email')
+    return zod.email({
+      message: `"Invalid email address"`,
+    });
+
+  if (property?.widget === 'url') return zod.url({ message: 'Invalid URL' });
+
+  if (property.maximum)
+    zod = zod.max(property.maximum, {
+      message: `Must be ${property.maximum} or low characters long `,
+    });
+
+  if (property.minimum)
+    zod = zod.min(property.minimum, {
+      message: `Must be ${property.minimum} or more characters long`,
+    });
+  return zod;
+};
+
+export const addNumberValidators = (property: TProperty) => {
+  let zod = z.number({
+    description: property?.helperText,
+    invalid_type_error: `${startCase(
+      property.label ?? property.id
+    )} should be a number`,
+    required_error: `${startCase(property.label ?? property.id)} is required!`,
+  });
+
+  if (property?.readOnly) return zod.readonly();
+
+  if (property?.required)
+    zod = zod.min(0, {
+      message: `${startCase(property?.label ?? property.id)} is required!`,
+    });
+
+  if (property.maximum)
+    zod = zod.max(property.maximum, {
+      message: `Must be ${property.maximum} or low characters long `,
+    });
+
+  if (property.minimum)
+    zod = zod?.min(property.minimum, {
+      message: `Must be ${property.minimum} or more characters long`,
+    });
+
+  return zod;
+};
+
+export const addBooleanValidators = (property: TProperty) => {
+  let zod = z.boolean({
+    description: property?.helperText,
+    invalid_type_error: `${startCase(
+      property.label ?? property.id
+    )} should be a boolean`,
+    required_error: `${startCase(property.label ?? property.id)} is required!`,
+  });
+
+  if (property?.readOnly) return zod.readonly();
+
+  return zod;
+};
+
+export const createRequiredSchema = (properties: TProperties) => {
+  let required: Record<string, any> = {};
+  Object.values(properties).forEach(property => {
+    if (property?.required) {
+      set(required, property.id, property.required);
+    }
+  });
+  return required;
+};
