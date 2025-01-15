803fc8f9182f9bcad58855f63d842663e14821dd
Sanjay Kumar Kumaravelan <sanjaykumarcena@gmail.com>
2025-01-15 19:57:56 +0100

    IATW add onError prop to DraupnirForm for error handling

--- src/core/draupnir-form.tsx
+++ src/core/draupnir-form.tsx
@@ -7,6 +7,7 @@ import { FieldGenerator } from './field-generator';
 
 type DraupnirInstanceProps = PropsWithChildren<{
   onSubmit: (values: unknown) => Promise<void> | void;
+  onError: (errors: unknown) => Promise<void> | void;
   className?: string;
   schema: TSchema;
 }>;
@@ -14,13 +15,17 @@ type DraupnirInstanceProps = PropsWithChildren<{
 const DraupnirForm = ({
   children,
   onSubmit,
+  onError,
   className,
   schema,
 }: DraupnirInstanceProps) => {
   const formProps = useFormContext();
 
   return (
-    <form onSubmit={formProps.handleSubmit(onSubmit)} className={className}>
+    <form
+      onSubmit={formProps.handleSubmit(onSubmit, onError)}
+      className={className}
+    >
       <FieldGenerator
         key={`root.fieldgenerator.${schema.title.toLowerCase()}`}
         properties={schema.properties}
